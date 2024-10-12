462c38f372d41b2338ef79ef0431f7648bdc123f
lousalvant <lousalvant@gmail.com>
2024-10-12 12:32:20 -0400

    Step 7

---
+++ Parks/ParkDetailView.swift
@@ -0,0 +1,31 @@
+//
+//  ParkDetailView.swift
+//  Parks
+//
+//  Created by Lou-Michael Salvant on 10/12/24.
+//
+
+import SwiftUI
+
+struct ParkDetailView: View {
+    
+    let park: Park // <-- park property to allow for passing in a park when the detail is presented
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) { // Aligns vertical views to the leading edge with 16pt spacing between views
+                Text(park.fullName)
+                    .font(.largeTitle)
+                Text(park.description)
+            }
+            .padding()
+
+            // TODO: Add horizontal scrolling images
+
+        }
+    }
+}
+
+#Preview {
+    ParkDetailView(park: Park.mocked)
+} 
--- Parks/ParksResponse.swift
+++ Parks/ParksResponse.swift
@@ -5,6 +5,8 @@
 //  Created by Lou-Michael Salvant on 10/12/24.
 //
 
+import Foundation
+
 struct ParksResponse: Codable {
     let data: [Park]
 }
@@ -24,3 +26,12 @@ struct ParkImage: Codable {
     let caption: String
     let url: String
 }
+
+extension Park {
+    static var mocked: Park {
+        let jsonUrl = Bundle.main.url(forResource: "park_mock", withExtension: "json")!
+        let data = try! Data(contentsOf: jsonUrl)
+        let park = try! JSONDecoder().decode(Park.self, from: data)
+        return park
+    }
+} 
