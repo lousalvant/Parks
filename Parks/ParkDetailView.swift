f69c5cfcd9e1261198ea5658146606eb685edeb3
lousalvant <lousalvant@gmail.com>
2024-10-12 12:41:42 -0400

    Step 8

--- Parks/ParkDetailView.swift
+++ Parks/ParkDetailView.swift
@@ -21,7 +21,31 @@ struct ParkDetailView: View {
             .padding()
 
             // TODO: Add horizontal scrolling images
-
+            ScrollView(.horizontal) { // <-- Create a horizontally scrolling scroll view
+                HStack(spacing: 16) { // <-- Use an HStack to arrange views horizontally with 16pt spacing between each view
+                    ForEach(park.images) { image in // <-- Add ForEach to iterate over the park's images.
+                        
+                        // TODO: Create Async Image view
+                        Rectangle()
+                                .aspectRatio(7/5, contentMode: .fit) // <-- Set aspect ratio 7:5
+                                .containerRelativeFrame(.horizontal, count: 9, span: 8, spacing: 16) // <-- Size the views relative to the container (spacing matches HStack spacing)
+                                .overlay {
+                                    AsyncImage(url: URL(string: image.url)) { image in
+                                        image
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                    } placeholder: {
+                                        Color(.systemGray4)
+                                    }
+                                }
+                                .cornerRadius(16)
+                        }
+                        .safeAreaPadding(.horizontal)
+                }
+                .scrollTargetLayout()
+            }
+            .scrollTargetBehavior(.viewAligned)
+            .scrollIndicators(.hidden)
         }
     }
 }
